ea965d09f0b929e167a7e5dc3271f29fcd6f0996
remon <remonmizanur@gmail.com>
2023-07-09 21:24:05 +0600

    print button

--- app/src/main/java/com/ALife/alife/Utils/Helpers.java
+++ app/src/main/java/com/ALife/alife/Utils/Helpers.java
@@ -2,14 +2,28 @@ package com.ALife.alife.Utils;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class Helpers {
@@ -41,5 +55,76 @@ public class Helpers {
 
         }
         return bitmap;
+    }
+
+    public static void createPDF(Bitmap bitmapPDF, Context context, String fileName, float width, float height) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        //float width = bitmapPDF.getWidth();//context.getResources().getDisplayMetrics().widthPixels;
+       // float height = bitmapPDF.getHeight();//context.getResources().getDisplayMetrics().heightPixels;
+
+        int convertWidth = (int) width, convertHeight = (int) height;
+
+        PdfDocument pdfDocument = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(convertWidth, convertHeight, 1).create();
+        PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        canvas.drawPaint(paint);
+
+
+        bitmapPDF = Bitmap.createScaledBitmap(bitmapPDF, convertWidth, convertHeight, true);
+        canvas.drawBitmap(bitmapPDF, 0, 0, null);
+        pdfDocument.finishPage(page);
+
+        String targetPDF;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            //dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + FolderName);
+
+            targetPDF = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toURI())+"/"+ fileName + new SimpleDateFormat("yyMMddHHmmss", Locale.getDefault()).format(new Date()) + ".pdf";
+        } else {
+            targetPDF = new File(Environment.getExternalStorageDirectory().toURI()) +"/"+ fileName + new SimpleDateFormat("yyMMddHHmmss", Locale.getDefault()).format(new Date()) + ".pdf";
+
+        }
+
+
+        File file;
+        file = new File(targetPDF);
+
+        try {
+            pdfDocument.writeTo(new FileOutputStream(file));
+            Toast.makeText(context, "Successfully saved at " + targetPDF, Toast.LENGTH_SHORT).show();
+            //openPDF(targetPDF, context);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("errorxx", e.getMessage());
+
+            pdfDocument.close();
+
+            Toast.makeText(context, "Successfully saved at Documents", Toast.LENGTH_SHORT).show();
+
+            //openPDF(targetPDF, context);
+        }
+    }
+
+    private static void openPDF(String path, Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(path)), "application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        context.startActivity(intent);
+    }
+
+    public static Bitmap loadBitmap(View v, int width, int height) {
+        Bitmap bitmapPDF = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmapPDF);
+        v.draw(canvas);
+
+        return bitmapPDF;
     }
 }
